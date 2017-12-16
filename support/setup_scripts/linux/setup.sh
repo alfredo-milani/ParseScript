9dabf48c0273062bd753f9ba48bfbb789f8a4cd0
Alfredo Milani <alfredo.milani.94@gmail.com>
2017-12-16 17:42:23 +0100

    minor fix

--- support/setup_scripts/linux/setup.sh
+++ support/setup_scripts/linux/setup.sh
@@ -4,7 +4,7 @@ declare -r remote_repo="https://github.com/alfredo-milani/ParseScript"
 declare -r tmp_path="/dev/shm"
 declare -r software_name="ParseScript"
 declare -r desktop_entry="ParseScript/support/setup_scripts/linux/parsescript.desktop"
-declare -r launcher_script="ParseScript/launchers/launcher_linux.sh"
+declare -r main_script="${software_name}/DataRetrieval.py"
 declare -r desktop_entry_sys_path="/usr/share/applications"
 declare -r software_sys_path="/opt"
 declare -r null="/dev/null"
@@ -25,8 +25,7 @@ if [ "${choise}" == "Yes" ]; then
 
     echo ">>>Spostamento ${software_name} in ${software_sys_path}"
     sudo rm -rf ${software_sys_path}/${software_name} &> ${null}
-    # Non necessario
-    # sudo chmod +x ${tmp_path}/${launcher_script}
+    sudo chmod +x ${tmp_path}/${main_script}
     sudo mv ${tmp_path}/${software_name} ${software_sys_path}
 
     echo ">>>Operazione completata. Riavviare il sistema o il Desktop Environment."
